f6cef07a704f2537d72a6357655e7548095378d4
gxy749 <gxy749@cs.bham.ac.uk>
2018-02-24 16:55:56 +0000

    Indent corrected

--- 1dhopper_withoutBeamNew.py
+++ 1dhopper_withoutBeamNew.py
@@ -67,94 +67,103 @@ if (useRealTimeSimulation):
 
 while 1:
     if (useRealTimeSimulation):
-        p.setGravity(0,0,-10)
-        sleep(0.01) # Time in seconds.
+        p.setGravity(0, 0, -10)
+        sleep(0.01)  # Time in seconds.
     else:
-        robotPos = p.getLinkState(boxId,0)[0]
-        p.resetDebugVisualizerCamera(5,0,-20,robotPos)
-        footLength = p.getJointState(boxId,1)[0]
-        # print("JointInfo:",footLength) #Get foot joint info
+        robotPos = p.getLinkState(boxId, 0)[0]
+        p.resetDebugVisualizerCamera(5, 0, -20, robotPos)
+        footLength = p.getJointState(boxId, 1)[0]
         springLength = abs(float(1) - abs(footLength))
         legLength = springLength + 0.5
         legLengthDifference = springLength - currentSpringLength
         currentSpringLength = springLength
-        if abs(legLengthDifference) < 0.000001: legLengthDifference = 0;
-        # print("legLengthDifference:",legLengthDifference)
+        if abs(legLengthDifference) < 0.000001:
+            legLengthDifference = 0
         stiffness = float(1)/springLength
         springHardness = 3000.0
-        #print("stiffness:", stiffness)
         legTorque = stiffness * float(abs(1 - springLength)) * springHardness
-        legAndBodyAngle = 1.57079632679 - p.getJointState(boxId,0)[0]
-        #print("legTorque:", legTorque)
-        toeLinkHeight = p.getLinkState(boxId,2)[0][2]
-        forwardVelocity = p.getLinkState(boxId,3,computeLinkVelocity=1)[7][2]
-        pitchAnglephiCurrent = math.atan((p.getLinkState(boxId,4)[4][2] - p.getLinkState(boxId,3)[4][2])/abs(p.getLinkState(boxId,4)[4][0] - p.getLinkState(boxId,3)[4][0]))
+        legAndBodyAngle = 1.57079632679 - p.getJointState(boxId, 0)[0]
+        toeLinkHeight = p.getLinkState(boxId, 2)[0][2]
+        forwardVelocity = p.getLinkState(boxId, 3, computeLinkVelocity=1)[7][2]
+        pitchAnglephiCurrent = math.atan((p.getLinkState(boxId, 4)[4][2] - p.getLinkState(boxId, 3)[4][2])/abs(p.getLinkState(boxId, 4)[4][0] - p.getLinkState(boxId, 3)[4][0]))
         pitchAnglePhiDerivative = pitchAnglephiCurrent - pitchAnglephi
         pitchAnglephi = pitchAnglephiCurrent
         # print(pitchAnglePhiDerivative)
         # print("pitchAngle:",pitchAnglephi)
         if abs(forwardVelocity) < 0.0000001:
-        	forwardVelocity = 0
+            forwardVelocity = 0
         if (toeLinkHeight > 0) and (toeLinkHeight < 0.2):
-        	if currentState == "FLIGHT":
-        		touchDownTime = time.time()
-        		# print("Touchdown")
-        	currentState = stateArray[0]
-        	toeOffTheGround = False
-        	# print("LOADING")
-        	if legLengthDifference < 0: currentState = stateArray[1];#Compression state
-        	if legLengthDifference > 0: currentState = stateArray[2]; hasThrusted = True;#Thrust state
-        	if (legLengthDifference == 0 and hasThrusted == True): currentState = stateArray[3]; hasThrusted = False;#ULOADING
-        	p.setJointMotorControl2(bodyUniqueId=boxId,jointIndex=1, controlMode=p.TORQUE_CONTROL, force = -legTorque)
-        	##GoodValue set
-        	controlBodyAttitude(physicsClass=p,phi=pitchAnglephi,phiDesired=0,phiDerivative=pitchAnglePhiDerivative,kp=1530,kv=140)
-
-        	# p.setJointMotorControl2(bodyUniqueId=boxId,jointIndex=3, controlMode=p.POSITION_CONTROL, targetPosition = 0,force = 500)
-        	# controlBodyAttitude(physicsClass=p,phi=pitchAnglephi,phiDesired=0,phiDerivative=pitchAnglePhiDerivative,kp=153,kv=14)
-        	#Trrque Test
-        	# p.setJointMotorControl2(bodyUniqueId=boxId,jointIndex=0, controlMode=p.TORQUE_CONTROL,force = -1000)
-
+            if currentState == "FLIGHT":
+                    touchDownTime = time.time()
+                    currentState = stateArray[0]
+                    toeOffTheGround = False
+            if legLengthDifference < 0:
+                currentState = stateArray[1]  # Compression state
+            if legLengthDifference > 0:
+                currentState = stateArray[2]
+                hasThrusted = True  # Thrust state
+            if (legLengthDifference == 0 and hasThrusted is True):
+                currentState = stateArray[3]
+                hasThrusted = False  # ULOADING
+            p.setJointMotorControl2(
+                        bodyUniqueId=boxId,
+                        jointIndex=1,
+                        controlMode=p.TORQUE_CONTROL,
+                        force=-legTorque
+                        )
+            controlBodyAttitude(
+                    physicsClass=p,
+                    phi=pitchAnglephi,
+                    phiDesired=0,
+                    phiDerivative=pitchAnglePhiDerivative,
+                    kp=1530,
+                    kv=140
+                    )
 
         else:
-        	if currentState == "THRUST":
-        		liftOffTime = time.time()
-        		stanceDuration = liftOffTime - touchDownTime
-        		# print("StanceDuration:",stanceDuration)
-        		stancePhaceTimeArray.append(liftOffTime - touchDownTime)
-        		# print("Average Stance Duration:",mean(stancePhaceTimeArray))
-        		# print("Lift off")
+            if currentState == "THRUST":
+                liftOffTime = time.time()
+                stanceDuration = liftOffTime - touchDownTime
+                # print("StanceDuration:",stanceDuration)
+                stancePhaceTimeArray.append(liftOffTime - touchDownTime)
+                # print("Average Stance Duration:",mean(stancePhaceTimeArray))
+                # print("Lift off")
         # 	# print("FLIGHT")
-        	toeOffTheGround = True
-        	currentState = stateArray[4]
-        	p.setJointMotorControl2(bodyUniqueId=boxId,jointIndex=1, controlMode=p.POSITION_CONTROL, targetPosition = 0.5,force = 600)
-        	#Position Leg For landing
-        	# try:
-        	averageStanceDuration = mean(stancePhaceTimeArray)
-        	desiredAngle = calculateDesiredLegAndBodyAngle(phi=pitchAnglephi,forwardSpeed=forwardVelocity,desiredForwardSpeed=0.2,stancePhaseDuration=averageStanceDuration,r=legLength,feedBackGain=0.5)
-        	if abs(desiredAngle) < 0.000001:
-        		desiredAngle = 0
-        	# print("DesiredAngle:",desiredAngle)
-        	# except:
-        		# print("error")
-        	if time.time() - startTime > 5:
-        		p.setJointMotorControl2(bodyUniqueId=boxId,jointIndex=0, controlMode=p.POSITION_CONTROL, targetPosition = desiredAngle,force = 200)
-        if currentState == "LOADING":pass;
-        	# print("Stop Exhausting leg, zero hip torque")
-        elif currentState == "COMPRESSION":pass;
-        	# p.setJointMotorControl2(bodyUniqueId=boxId,jointIndex=4, controlMode=p.TORQUE_CONTROL, force = -legTorque)
-        	# print("Seal Upper Leg Chamber")
-        elif currentState == "THRUST":pass;
-        	# print("THRUST")
-        	# p.setJointMotorControl2(bodyUniqueId=boxId,jointIndex=4, controlMode=p.TORQUE_CONTROL, force = -legTorque)
-        elif currentState == "UNLOADING":pass;
-        	# print("Stop thrust, zero hip angle")
-        elif currentState == "FLIGHT":pass;
-        	# print("Exhaust leg to low pressure")
-        # print(currentState)
-        # if abs(forwardVelocity)>0.00001:
-        # 	print("forwardSpeed:",forwardVelocity)
+            toeOffTheGround = True
+            currentState = stateArray[4]
+            p.setJointMotorControl2(
+                        bodyUniqueId=boxId,
+                        jointIndex=1,
+                        controlMode=p.POSITION_CONTROL,
+                        targetPosition=0.5,
+                        force=600
+                        )
+            averageStanceDuration = mean(stancePhaceTimeArray)
+            desiredAngle = calculateDesiredLegAndBodyAngle(
+                    phi=pitchAnglephi,
+                    forwardSpeed=forwardVelocity,
+                    desiredForwardSpeed=0.2,
+                    stancePhaseDuration=averageStanceDuration,
+                    r=legLength,
+                    feedBackGain=0.5)
+            if abs(desiredAngle) < 0.000001:
+                desiredAngle = 0
+            if time.time() - startTime > 5:
+                p.setJointMotorControl2(
+                        bodyUniqueId=boxId,
+                        jointIndex=0,
+                        controlMode=p.POSITION_CONTROL,
+                        targetPosition=desiredAngle,
+                        force=200
+                        )
+        if currentState == "LOADING":
+            pass
+        elif currentState == "COMPRESSION":
+            pass
+        elif currentState == "THRUST":
+            pass
+        elif currentState == "UNLOADING":
+            pass
+        elif currentState == "FLIGHT":
+            pass
         p.stepSimulation()
-        # print(pitchAnglephi)
-        # print(p.getJointState(boxId,2)[0])
-        # print(pitchAnglePhiDerivative)
-        # print(legAndBodyAngle)
